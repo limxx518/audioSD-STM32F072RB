3fc5f7ec288ca797e094ffcd49b6d193d755b287
limxx518 <limxx518@umn.edu>
2020-09-06 19:49:56 -0500

    Removed unnecessary files

--- Output/audioSD_STM32F072RB Debug/Obj/SEGGER_RTT.d
+++
@@ -1,8 +0,0 @@
-Output/audioSD_STM32F072RB\ Debug/Obj/SEGGER_RTT.o: \
- C:\Users\Jan-Wei\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\audioSD_STM32F072RB\SEGGER_RTT.c \
- C:\Users\Jan-Wei\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\audioSD_STM32F072RB\SEGGER_RTT.h \
- C:\Users\Jan-Wei\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\audioSD_STM32F072RB\SEGGER_RTT_Conf.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/stdlib.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/__crossworks.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/stdarg.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.52b/include/string.h
--- Output/audioSD_STM32F072RB Debug/Obj/STM32F0x2_Vectors.d
+++
@@ -1,2 +0,0 @@
-Output/audioSD_STM32F072RB\ Debug/Obj/STM32F0x2_Vectors.o: \
- C:\Users\Jan-Wei\Documents\SEGGER\ Embedded\ Studio\ for\ ARM\ Projects\audioSD_STM32F072RB\STM32F0xx\Source\STM32F0x2_Vectors.s
